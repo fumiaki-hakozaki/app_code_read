6ff0bf9bc8a0c73efee6cab0a29f256053150618
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2023-01-04 23:36:38 +0900

    ブランチ修正

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,5 @@ Rails.application.routes.draw do
     end
   end
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/inbox" if Rails.env.development?
 end
